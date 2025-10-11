d535801f095365f3f022241d828817dc2f484f32
d4rk-h <herreragp334@gmail.com>
2025-10-11 13:25:52 +0100

    feat: utils/ DatalakePathBuilder class developed

--- ingestion-service/src/main/java/control/utils/DatalakePathBuilder.java
+++ ingestion-service/src/main/java/control/utils/DatalakePathBuilder.java
@@ -1,4 +1,17 @@
 package control.utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DatalakePathBuilder {
+    private final String path = "datalake/";
+
+    private String createBookDir() {
+        String datalakePath;
+        LocalDateTime timestamp = LocalDateTime.now();
+        String dateStr = timestamp.format(DateTimeFormatter.ofPattern("%Y%m%d"));
+        String hourStr = timestamp.format(DateTimeFormatter.ofPattern("%H"));
+        datalakePath = path + dateStr + "/" + hourStr + "/";
+        return datalakePath;
+    }
 }
