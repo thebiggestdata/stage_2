265ebe9f42b0dbbc97b77cc366ead75ce68e0860
d4rk-h <herreragp334@gmail.com>
2025-10-24 18:22:14 +0100

    feat: api/config developed OpenApiConfig as well as IngestionApiConfig classes for the configuration of the ingestion service rest api

---
+++ ingestion-service/src/main/java/api/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI quantumComputingOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("Dev server");
+        Contact contact = new Contact();
+        contact.setUrl("https://github.com/thebiggestdata/stage_2");
+        contact.setName("The biggest data team");
+        Info info = new Info()
+                .title("Ingestion Service API")
+                .version("1.0.0")
+                .contact(contact)
+                .description("REST API for the ingestion service of a search engine");
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer));
+    }
+}
