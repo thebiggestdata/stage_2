536812d1e63884e83804c546b8740c7e99e0dea2
d4rk-h <herreragp334@gmail.com>
2025-10-24 19:35:54 +0100

    feat: api/ create the SpringBootAplication, main class of the api

--- ingestion-service/src/main/java/api/IngestionApiApplication.java
+++ ingestion-service/src/main/java/api/IngestionApiApplication.java
@@ -1,4 +1,14 @@
 package api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"api", "control", "model"})
 public class IngestionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestionApiApplication.class, args);
+    }
 }
