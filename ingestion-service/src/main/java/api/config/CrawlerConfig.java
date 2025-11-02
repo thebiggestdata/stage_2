e7712c3cabded9c019e4f8c8f8e63d17e0e7831f
d4rk-h <herreragp334@gmail.com>
2025-11-02 11:13:11 +0000

    fix: add the data lake path from .properties file

--- ingestion-service/src/main/java/api/config/CrawlerConfig.java
+++ ingestion-service/src/main/java/api/config/CrawlerConfig.java
@@ -1,15 +1,19 @@
 package api.config;
 
 import control.CrawlerController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CrawlerConfig {
 
+    @Value("${datalake.base.path}")
+    private String datalakeBasePath;
+
     @Bean
     public CrawlerController crawlerController() {
         control.utils.CrawlerConfig config = new control.utils.CrawlerConfig(1, 1000, 1000L);
-        return new CrawlerController(config);
+        return new CrawlerController(config, datalakeBasePath);
     }
 }
