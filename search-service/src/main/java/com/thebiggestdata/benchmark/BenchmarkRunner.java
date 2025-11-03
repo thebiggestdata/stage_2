fd51959b3c3e0786cf6f245e50f35648492c418e
Miguel Cabeza <migcab86@gmail.com>
2025-11-03 20:47:48 +0000

    fix of the benchmark and export to csv

--- search-service/src/main/java/com/thebiggestdata/benchmark/BenchmarkRunner.java
+++ search-service/src/main/java/com/thebiggestdata/benchmark/BenchmarkRunner.java
@@ -1,9 +1,19 @@
 package com.thebiggestdata.benchmark;
 
-import org.openjdk.jmh.Main;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.results.format.ResultFormatType;
 
 public class BenchmarkRunner {
+
 	public static void main(String[] args) throws Exception {
-		Main.main(args);
+		Options opt = new OptionsBuilder()
+				.include("com.thebiggestdata.benchmark.*")
+				.result("benchmark-result.csv")
+				.resultFormat(ResultFormatType.CSV)
+				.build();
+
+		new Runner(opt).run();
 	}
 }
