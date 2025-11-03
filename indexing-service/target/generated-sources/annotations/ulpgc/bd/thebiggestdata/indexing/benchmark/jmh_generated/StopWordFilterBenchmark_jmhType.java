c84fea178e00d4ce525e9ed4cb0ddb318f054fe5
herna <oscarpitti@gmail.com>
2025-11-03 00:02:11 +0000

    Clean Benchmark indexing-service

--- indexing-service/target/generated-sources/annotations/ulpgc/bd/thebiggestdata/indexing/benchmark/jmh_generated/StopWordFilterBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package ulpgc.bd.thebiggestdata.indexing.benchmark.jmh_generated;
-public class StopWordFilterBenchmark_jmhType extends StopWordFilterBenchmark_jmhType_B3 {
-}
-
--- indexing-service/target/generated-sources/annotations/ulpgc/bd/thebiggestdata/indexing/benchmark/jmh_generated/TokenizerBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package ulpgc.bd.thebiggestdata.indexing.benchmark.jmh_generated;
-public class TokenizerBenchmark_jmhType extends TokenizerBenchmark_jmhType_B3 {
-}
-
